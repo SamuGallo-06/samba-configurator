068f1b0f017c1d3fd3b3cf8aca3977566584dfce
SamuGallo-06 <samu.gallicani@gmail.com>
2025-12-11 13:35:23 +0100

    Removed Printer dataclass from shared_elements.py

--- shared_elements.py
+++ shared_elements.py
@@ -12,15 +12,6 @@ class SharedFolder:
     create_mask: str = "0777"
     directory_mask: str = "0777"
     browseable: bool = True
-    writeable: bool = True 
-
-@dataclass
-class SharedPrinter:
-    comment: str = "Shared Printer"
-    name: str = ""
-    path: str = ""
-    read_only: bool = False
-    guest_ok: bool = False
-    valid_users: list = field(default_factory=list)
+    writeable: bool = True
 
 
